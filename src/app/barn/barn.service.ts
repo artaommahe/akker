569606893adfcc20ebeb16459fb0b5be7a775f23
Popov Max <drowpmv@gmail.com>
2024-06-06 13:34:39 +0200

    feat(barn): add sprouts to barn and implement seed conversion to sprout

--- src/app/barn/barn.service.ts
+++ src/app/barn/barn.service.ts
@@ -3,6 +3,7 @@ import { Injectable, computed, effect, signal } from '@angular/core';
 interface Barn {
   version: number;
   seeds: Record<string, Seed>;
+  sprouts: Sprout[];
 }
 
 export interface Seed {
@@ -12,19 +13,41 @@ export interface Seed {
   lastAddedAt: number;
 }
 
+export interface Sprout {
+  name: string;
+  addedAt: number;
+}
+
 @Injectable({ providedIn: 'root' })
 export class BarnService {
-  private barn = signal<Barn>({ version: 1, seeds: {} });
+  private barn = signal<Barn>({ version: 1, seeds: {}, sprouts: [] });
 
   seeds = computed(() => this.barn().seeds);
+  sprouts = computed(() => this.barn().sprouts);
 
   constructor() {
     this.initStorage();
   }
 
   addSeed(name: string) {
+    const sprout = this.barn().sprouts.find(sprout => sprout.name === name);
+
+    // if the seed is already sprouted, don't add it to the seeds
+    if (sprout) {
+      return;
+    }
+
+    let seed: Seed | undefined = this.barn().seeds[name];
+
+    // if the count of this seed is greater than or equal to the treshold, convert it to a sprout
+    if (seed?.count >= seedPlantingTreshold - 1) {
+      this.addSprout(name);
+      this.removeSeed(name);
+      return;
+    }
+
     this.barn.update(barn => {
-      const seed = barn.seeds[name] ?? { name, count: 0, addedAt: Date.now(), lastAddedAt: Date.now() };
+      seed ??= { name, count: 0, addedAt: Date.now(), lastAddedAt: Date.now() };
 
       return {
         ...barn,
@@ -44,6 +67,20 @@ export class BarnService {
     });
   }
 
+  removeSprout(name: string) {
+    this.barn.update(barn => ({
+      ...barn,
+      sprouts: barn.sprouts.filter(sprout => sprout.name !== name),
+    }));
+  }
+
+  private addSprout(name: string) {
+    this.barn.update(barn => ({
+      ...barn,
+      sprouts: [...barn.sprouts, { name, addedAt: Date.now() }],
+    }));
+  }
+
   private initStorage() {
     const barn = localStorage.getItem(barnStorageKey);
 
@@ -60,3 +97,4 @@ export class BarnService {
 }
 
 const barnStorageKey = 'barn';
+const seedPlantingTreshold = 5;
