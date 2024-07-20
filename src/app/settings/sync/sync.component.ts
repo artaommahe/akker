f9f808bdac65d74e95b4854b99a994233f521134
Popov Max <drowpmv@gmail.com>
2024-07-20 12:12:10 +0200

    feat(sync): add backup resore, simplify backup downloading

--- src/app/settings/sync/sync.component.ts
+++ src/app/settings/sync/sync.component.ts
@@ -1,40 +1,83 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ButtonDirective } from '../../ui/button/button';
 import { BarnDbService } from '../../barn/barnDb.service';
+import { IconComponent } from '../../ui/icon/icon';
+import cross from '../../ui/modal/assets/cross.svg';
+import { provideIcons } from '../../ui/icon/provide-icons';
 
 @Component({
   selector: 'app-sync',
   template: `
-    <div class="flex items-center gap-4">
-      <button appButton (click)="export()">Export data</button>
-
-      @let data = barnData();
-      @if (data) {
-        <a
-          class="text-action-primary underline"
-          [href]="'data:text/plain;charset=utf-8,' + data.fileContent"
-          [download]="data.fileName"
-        >
-          download
-        </a>
-      }
+    <div class="flex items-center justify-between gap-4">
+      <button appButton (click)="backup()">Backup data</button>
+      <button appButton (click)="showRestoreDialog.set(true)">Restore backup</button>
     </div>
+
+    <!-- TODO: ui/dialog -->
+    @if (showRestoreDialog()) {
+      <div class="fixed inset-0 bg-primary p-5">
+        <!-- TODO: ui/button -->
+        <button class="absolute right-2 top-2 p-2" (click)="showRestoreDialog.set(false)">
+          <app-icon class="size-6 text-secondary" name="cross" />
+        </button>
+
+        <div class="flex h-full flex-col gap-6 pt-8">
+          <p class="text-lg">
+            <span class="text-semantic-warning">WARNING:</span>
+            restored data will be added on top of existing data
+          </p>
+
+          <label class="flex flex-col gap-2 text-secondary">
+            Select backup file
+            <input type="file" accept=".json" #backupFile />
+          </label>
+
+          <div class="mt-auto flex items-center justify-end gap-4">
+            <button appButton (click)="showRestoreDialog.set(false)">Cancel</button>
+            <button appButton appButtonType="warning" (click)="restore(backupFile)">Restore</button>
+          </div>
+        </div>
+      </div>
+    }
   `,
-  imports: [ButtonDirective],
+  imports: [ButtonDirective, IconComponent],
+  providers: [provideIcons({ cross })],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SyncComponent {
   private barnDb = inject(BarnDbService);
 
-  barnData = signal<{ fileName: string; fileContent: string } | null>(null);
+  showRestoreDialog = signal(false);
 
-  async export() {
+  async backup() {
     const data = await this.barnDb.exportJSON();
 
-    this.barnData.set({
-      fileName: `akker-export-data-${new Date().toISOString()}.json`,
-      fileContent: encodeURIComponent(JSON.stringify(data)),
-    });
+    const dataLink = document.createElement('a');
+    dataLink.href = `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`;
+    dataLink.download = `akker-export-data-${new Date().toISOString()}.json`;
+    dataLink.click();
+  }
+
+  async restore(backupFileInput: HTMLInputElement) {
+    if (!backupFileInput.files?.[0]) {
+      return;
+    }
+
+    const fileContent = await readFileTextContent(backupFileInput.files[0]);
+    const data = JSON.parse(fileContent);
+
+    await this.barnDb.importJSON(data);
+
+    this.showRestoreDialog.set(false);
   }
 }
+
+const readFileTextContent = (file: File) => {
+  return new Promise<string>((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => resolve(fileReader.result as string);
+    fileReader.onerror = () => reject(fileReader.error);
+    fileReader.readAsText(file);
+  });
+};
