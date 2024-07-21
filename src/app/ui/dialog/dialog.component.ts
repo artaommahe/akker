15af92fb580876ba91d0e73f31e540d27d571ae6
Popov Max <drowpmv@gmail.com>
2024-07-21 12:57:22 +0200

    feat(ui): extract simple dialog component

---
+++ src/app/ui/dialog/dialog.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { IconComponent } from '../icon/icon';
+
+@Component({
+  selector: 'app-dialog',
+  template: `
+    <div class="fixed inset-0 bg-primary p-5">
+      <!-- TODO: ui/button -->
+      <button class="absolute right-2 top-2 p-2" (click)="close.emit()">
+        <app-icon class="size-6 text-secondary" name="crossInCircle" />
+      </button>
+
+      <ng-content></ng-content>
+    </div>
+  `,
+  imports: [IconComponent],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogComponent {
+  close = output();
+}
