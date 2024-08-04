cc21ec02b3c0016208e7aaa1ebf40728fce9ef3a
Maksim Popov <drowpmv@gmail.com>
2024-08-04 13:58:01 +0200

    chore: setup sentry (#2)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { type ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { type ApplicationConfig, ErrorHandler, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideBarnDbAsync } from './barn/barnDb.service';
+import { createErrorHandler } from '@sentry/angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideBarnDbAsync()],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideBarnDbAsync(),
+    {
+      provide: ErrorHandler,
+      useValue: createErrorHandler({ showDialog: false }),
+    },
+  ],
 };
