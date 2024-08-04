1122937e13ab6ce575bd2c3ac164b2f77e26819c
Popov Max <drowpmv@gmail.com>
2024-08-04 13:57:16 +0200

    chore: setup sentry

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  sentry: {
+    dsn: 'https://8581fd3433617ae927dc974836ef34d8@o4507718881837056.ingest.de.sentry.io/4507718884458576',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { init } from '@sentry/angular';
+import { environment } from './environments/environment';
+
+init({
+  enabled: !!environment.sentry.dsn,
+  dsn: environment.sentry.dsn,
+  integrations: [],
+});
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
