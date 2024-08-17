df8195d7421d6301fdeb98d3fb81ad0c6649712c
Popov Max <drowpmv@gmail.com>
2024-08-17 13:32:59 +0200

    chore(actions): run lint relatively to a main branch, don't run lint on main branch

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,7 @@ const isCheckMode = process.env.LINT_CHECK === 'true';
 
 export default {
   '*': [
-    `eslint --no-warn-ignored ${!isCheckMode && '--fix'}`,
+    `eslint --no-warn-ignored ${isCheckMode ? '' : '--fix'}`,
     `prettier --ignore-unknown ${isCheckMode ? '--check' : '--write'}`,
   ],
 };
