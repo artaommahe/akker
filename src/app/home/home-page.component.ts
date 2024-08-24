88f40535c0169964991d1a916ffab144b061e358
Popov Max <drowpmv@gmail.com>
2024-08-24 12:58:47 +0200

    feat(home): add a learn button to the home page

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LearnCardsComponent } from '../learning/learn-cards.component';
 import { UnsortedSproutsComponent } from './unsorted-sprouts/unsorted-sprouts.component';
 
 @Component({
@@ -7,9 +8,11 @@ import { UnsortedSproutsComponent } from './unsorted-sprouts/unsorted-sprouts.co
   template: `
     <div class="flex flex-col gap-4">
       <app-unsorted-sprouts />
+
+      <app-learn-cards />
     </div>
   `,
-  imports: [UnsortedSproutsComponent],
+  imports: [UnsortedSproutsComponent, LearnCardsComponent],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
