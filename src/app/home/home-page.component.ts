0df659000bdf08e7df9bc4e3e04136f4a0e80121
Popov Max <drowpmv@gmail.com>
2024-08-24 12:46:50 +0200

    feat(home): add unsorted sprouts list

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -1,9 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { UnsortedSproutsComponent } from './unsorted-sprouts/unsorted-sprouts.component';
+
 @Component({
   selector: 'app-home-page',
-  template: ``,
-  imports: [],
+  template: `
+    <div class="flex flex-col gap-4">
+      <app-unsorted-sprouts />
+    </div>
+  `,
+  imports: [UnsortedSproutsComponent],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
