57ea0b801d2a7c446643b603acd0669998606283
Popov Max <drowpmv@gmail.com>
2024-09-21 13:01:00 +0200

    fix(seeds): fix state reset on add seeds dialog re-open, fix components naming

---
+++ src/app/seeds/add-seeds-button/add-seeds-button.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+import { DialogComponent } from '../../ui/dialog/dialog.component';
+import { IconComponent } from '../../ui/icon/icon';
+import { AddSeedsComponent } from '../add-seeds/add-seeds.component';
+
+@Component({
+  selector: 'app-add-seeds-button',
+  template: `
+    <button
+      class="fixed bottom-16 right-6 flex items-center justify-center rounded-full bg-action-primary shadow-md"
+      title="Add"
+      (click)="showAddSeedsDialog.set(true)"
+    >
+      <app-icon class="size-10 text-primary" name="plusInCircle" />
+    </button>
+
+    <app-dialog [open]="showAddSeedsDialog()" (close)="showAddSeedsDialog.set(false)">
+      <ng-template>
+        <app-add-seeds (close)="showAddSeedsDialog.set(false)" />
+      </ng-template>
+    </app-dialog>
+  `,
+  imports: [IconComponent, AddSeedsComponent, DialogComponent],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddSeedsButtonComponent {
+  showAddSeedsDialog = signal(false);
+}
