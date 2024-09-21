1523d434d2fbf10de3d942ebb462e16aa2266ee5
Popov Max <drowpmv@gmail.com>
2024-09-21 12:52:42 +0200

    fix(ui/dialog): fix re-creating content durring closing animation

--- src/app/ui/dialog/dialog.component.ts
+++ src/app/ui/dialog/dialog.component.ts
@@ -4,14 +4,14 @@ import {
   Component,
   ElementRef,
   TemplateRef,
-  computed,
   contentChild,
   effect,
   input,
   output,
-  signal,
   viewChild,
 } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest, fromEvent, map, of, switchMap } from 'rxjs';
 
 import { IconComponent } from '../icon/icon';
 
@@ -26,8 +26,6 @@ import { IconComponent } from '../icon/icon';
       ]"
       [inert]="!isOpen()"
       (close)="close.emit()"
-      (transitionstart)="dialogAnimationIsRunning.set(true)"
-      (transitionend)="dialogAnimationIsRunning.set(false)"
       #dialog
     >
       <!-- TODO: ui/button -->
@@ -51,9 +49,13 @@ export class DialogComponent {
   dialogRef = viewChild.required<ElementRef<HTMLDialogElement>>('dialog');
   contentRef = contentChild.required(TemplateRef);
 
-  // have to wait for close animation to finish before removing dialog's content from the DOM
-  dialogAnimationIsRunning = signal(false);
-  isOpen = computed(() => (this.openInput() ? true : this.dialogAnimationIsRunning()));
+  isOpen = toSignal(
+    combineLatest([toObservable(this.openInput), toObservable(this.dialogRef)]).pipe(
+      switchMap(([open, dialogRef]) =>
+        open ? of(open) : fromEvent(dialogRef.nativeElement, 'transitionend').pipe(map(() => false)),
+      ),
+    ),
+  );
 
   constructor() {
     effect(() => {
