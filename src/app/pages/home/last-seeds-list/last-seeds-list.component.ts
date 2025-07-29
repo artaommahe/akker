1513a7dddf5596c533acb595019729aa64965e2e
Maksim Popov <drowpmv@gmail.com>
2025-07-29 13:11:07 +0200

    feat(seeds, home): use new SeedsList component

--- src/app/pages/home/last-seeds-list/last-seeds-list.component.ts
+++ src/app/pages/home/last-seeds-list/last-seeds-list.component.ts
@@ -1,9 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { SeedsListComponent } from 'src/app/seeds/seeds-list/seeds-list.component';
 import { SeedsService } from 'src/app/seeds/seeds.service';
-
-import { SeedDetailsDialogComponent } from '../../../seeds/seed-details-dialog/seed-details-dialog.component';
-import { type SeedDetailsSeed } from '../../../seeds/seed-details/seed-details.component';
-import { SeedsListItemComponent } from '../../../seeds/seeds-list-item/seeds-list-item.component';
 
 @Component({
   selector: 'app-last-seeds-list',
@@ -11,44 +8,21 @@ import { SeedsListItemComponent } from '../../../seeds/seeds-list-item/seeds-lis
     <section class="flex flex-col gap-2">
       <h2 class="text-secondary text-lg" id="last-seeds-heading">Last seeds</h2>
 
-      @switch (lastSeeds.status()) {
-        @case ('loading') {
-          <p>Loading...</p>
-        }
-        @case ('error') {
-          <p class="text-semantic-danger">Error loading last seeds list:</p>
-          <p>{{ lastSeeds.error() }}</p>
-        }
-        @default {
-          <ul class="flex flex-col gap-2" aria-labelledby="last-seeds-heading">
-            @for (seed of lastSeeds.value(); track seed.name) {
-              <li>
-                <app-seeds-list-item [seed]="seed" (showDetails)="seedDetailsDialog.set({ open: true, seed })" />
-              </li>
-            }
-          </ul>
-        }
-      }
+      <app-seeds-list
+        [seeds]="lastSeeds.value()"
+        [isLoading]="lastSeeds.isLoading()"
+        [loadingError]="lastSeeds.error()"
+        [listAriaLabelledBy]="'last-seeds-heading'"
+      />
     </section>
-
-    <app-seed-details-dialog
-      [open]="seedDetailsDialog().open"
-      [seed]="seedDetailsDialog().seed"
-      (dismiss)="closeSeedDetailsDialog()"
-    />
   `,
-  imports: [SeedDetailsDialogComponent, SeedsListItemComponent],
+  imports: [SeedsListComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LastSeedsListComponent {
   private seedsService = inject(SeedsService);
 
   lastSeeds = this.seedsService.getSeeds({ limit: lastSeedsCount });
-  seedDetailsDialog = signal<{ open: boolean; seed: SeedDetailsSeed | null }>({ open: false, seed: null });
-
-  closeSeedDetailsDialog() {
-    this.seedDetailsDialog.update(value => ({ ...value, open: false }));
-  }
 }
 
 const lastSeedsCount = 10;
--- src/app/pages/seeds/seeds-page.component.ts
+++ src/app/pages/seeds/seeds-page.component.ts
@@ -1,50 +1,67 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { SeedsListComponent } from 'src/app/seeds/seeds-list/seeds-list.component';
 import { SeedsService } from 'src/app/seeds/seeds.service';
+import { ExpansionPanelComponent } from 'src/app/ui/expansion-panel/expansion-panel.component';
 
 import { AddSeedsButtonComponent } from '../../seeds/add-seeds-button/add-seeds-button.component';
-import { SeedDetailsDialogComponent } from '../../seeds/seed-details-dialog/seed-details-dialog.component';
-import type { SeedDetailsSeed } from '../../seeds/seed-details/seed-details.component';
-import { SeedsListComponent } from './seeds-list/seeds-list.component';
 
 @Component({
   selector: 'app-seeds-page',
   template: `
     <div class="flex flex-col gap-4">
-      @switch (newSeeds.status()) {
-        @case ('loading') {
-          <p>Loading...</p>
-        }
-        @case ('error') {
-          <p class="text-semantic-danger">Error loading seeds list:</p>
-          <p>{{ newSeeds.error() }}</p>
-        }
-        @default {
+      <div class="flex gap-4">
+        <section class="min-w-0 flex-1">
+          <h2 class="text-secondary text-lg" id="top-seeds-heading">Top seeds</h2>
+
           <app-seeds-list
-            [seeds]="newSeeds.value() ?? []"
-            (showDetails)="seedDetailsDialog.set({ open: true, seed: $event })"
+            [seeds]="topSeeds()"
+            [isLoading]="seeds.isLoading()"
+            [loadingError]="seeds.error()"
+            [listAriaLabelledBy]="'top-seeds-heading'"
           />
-        }
-      }
+        </section>
+
+        <div class="border-l-primary/10 w-px self-stretch border-l"></div>
+
+        <section class="min-w-0 flex-1">
+          <h2 class="text-secondary text-lg" id="last-seeds-heading">Last seeds</h2>
+
+          <app-seeds-list
+            [seeds]="lastAddedSeeds()"
+            [isLoading]="seeds.isLoading()"
+            [loadingError]="seeds.error()"
+            [listAriaLabelledBy]="'last-seeds-heading'"
+          />
+        </section>
+      </div>
+
+      <app-expansion-panel>
+        <ng-container>All seeds ({{ sortedSeeds().length }})</ng-container>
+
+        <ng-template>
+          <app-seeds-list
+            [seeds]="sortedSeeds()"
+            [isLoading]="seeds.isLoading()"
+            [loadingError]="seeds.error()"
+            [listAriaLabel]="'All seeds'"
+          />
+        </ng-template>
+      </app-expansion-panel>
 
       <app-add-seeds-button />
-
-      <app-seed-details-dialog
-        [open]="seedDetailsDialog().open"
-        [seed]="seedDetailsDialog().seed"
-        (dismiss)="closeSeedDetailsDialog()"
-      />
     </div>
   `,
-  imports: [SeedsListComponent, SeedDetailsDialogComponent, AddSeedsButtonComponent],
+  imports: [SeedsListComponent, AddSeedsButtonComponent, ExpansionPanelComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SeedsPageComponent {
   private seedsService = inject(SeedsService);
 
-  newSeeds = this.seedsService.getSeeds();
-  seedDetailsDialog = signal<{ open: boolean; seed: SeedDetailsSeed | null }>({ open: false, seed: null });
+  seeds = this.seedsService.getSeeds();
 
-  closeSeedDetailsDialog() {
-    this.seedDetailsDialog.update(value => ({ ...value, open: false }));
-  }
+  sortedSeeds = computed(() => this.seeds.value()?.toSorted((a, b) => b.count - a.count) ?? []);
+  topSeeds = computed(() => this.sortedSeeds()?.slice(0, topSeedsCount));
+  lastAddedSeeds = computed(() => this.seeds.value()?.slice(0, topSeedsCount) ?? []);
 }
+
+const topSeedsCount = 10;
