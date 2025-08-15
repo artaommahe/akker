90a5c18f66518b5e8aeda60c1fbcc101c1415561
Maksim Popov <drowpmv@gmail.com>
2025-08-15 13:50:56 +0200

    feat(storybook): add a11y addon (#120)
    * feat(storybook): add a11y addon
    
    * feat(storybook): don't open storybook automatically, suppress compodoc output

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@analogjs/storybook-angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: {
     name: '@analogjs/storybook-angular',
     options: {},
