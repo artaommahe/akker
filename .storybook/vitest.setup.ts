446e2696fadb366ef02a2963f7bdef20a2852391
Maksim Popov <drowpmv@gmail.com>
2025-08-17 11:32:08 +0200

    feat(storybook): update analogjs' storybook and vitest libs, merge storybook tests setup to main vite.cofig

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,6 +1,9 @@
-// Replace your-framework with the framework you are using, e.g. react-vite, nextjs, nextjs-vite, etc.
-import { setProjectAnnotations } from '@analogjs/storybook-angular';
+import { setProjectAnnotations } from '@analogjs/storybook-angular/testing';
+import { beforeAll } from 'vitest';
 
 import * as previewAnnotations from './preview';
 
-setProjectAnnotations([previewAnnotations]);
+const annotations = setProjectAnnotations([previewAnnotations]);
+
+// Run Storybook's beforeAll hook
+beforeAll(annotations.beforeAll);
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,57 @@
 /// <reference types="vitest" />
 /// <reference types="@vitest/browser/providers/playwright" />
 import angular from '@analogjs/vite-plugin-angular';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [angular(), tsconfigPaths()],
     test: {
-      globals: true,
-      setupFiles: ['src/test-setup.ts'],
-      include: ['src/**/*.spec.ts'],
       reporters: ['default'],
       pool: 'threads',
-      browser: {
-        enabled: true,
-        provider: 'playwright',
-        headless: true,
-        instances: [{ browser: 'chromium' }],
-        screenshotFailures: false,
-      },
+      projects: [
+        {
+          extends: true,
+          test: {
+            name: 'unit',
+            globals: true,
+            setupFiles: ['src/test-setup.ts'],
+            include: ['src/**/*.spec.ts'],
+            browser: {
+              enabled: true,
+              provider: 'playwright',
+              headless: true,
+              instances: [{ browser: 'chromium' }],
+              screenshotFailures: false,
+            },
+          },
+        },
+        {
+          extends: true,
+          plugins: [
+            storybookTest({
+              configDir: path.join(dirname, '.storybook'),
+            }),
+          ],
+          test: {
+            name: 'storybook',
+            setupFiles: ['./.storybook/vitest.setup.ts'],
+            browser: {
+              enabled: true,
+              provider: 'playwright',
+              headless: true,
+              instances: [{ browser: 'chromium' }],
+            },
+          },
+        },
+      ],
     },
     define: { 'import.meta.vitest': mode !== 'production' },
   };
