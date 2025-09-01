c2ac50aaa88607653d0c8ea665ba08bb268e2164
Maksim Popov <drowpmv@gmail.com>
2025-09-01 13:27:09 +0200

    fix(storybook): add missed a11y vitest setup

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,9 +1,10 @@
 import { setProjectAnnotations } from '@analogjs/storybook-angular/testing';
+import * as addonA11y from '@storybook/addon-a11y/preview';
 import { beforeAll } from 'vitest';
 
 import * as previewAnnotations from './preview';
 
-const annotations = setProjectAnnotations([previewAnnotations]);
+const annotations = setProjectAnnotations([previewAnnotations, addonA11y]);
 
 // Run Storybook's beforeAll hook
 beforeAll(annotations.beforeAll);
